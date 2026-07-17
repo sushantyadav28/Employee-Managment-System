4aba2ad40a577b6f4f0a4527d7b84ca55a52013b
Sushant Yadav <sy7344678@gmail.com>
2026-07-17 21:56:04 +0530

    .

--- Backend/server.js
+++ Backend/server.js
@@ -11,6 +11,8 @@ app.use(cors());
 app.use(express.json());
 app.use("/api", employeeRoutes);
 
+
+
 connectDB();
 
 app.get("/", (req, res) => {
--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:4000/api";
+// const BASE_URL = "https://employee-managment-system-cv3c.onrender.com/";
+
 
 export const createEmployee = (data) =>
   axios.post(`${BASE_URL}/createemployee`, data);
